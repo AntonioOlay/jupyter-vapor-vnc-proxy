54d58c2f51316feea64d494ccdf783714f0ee0da
Antonio Olaya <138058637+AntonioOlay@users.noreply.github.com>
2024-05-22 11:02:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setuptools.setup(
     name="jupyter-vapor-vnc-proxy",
     packages=setuptools.find_packages(where='.'),
-    version='0.0.2',
+    version='0.0.3',
     description="Extension de Jupyter que ejecuta NCAR Vapor desde VNC",
     keywords=["Jupyter"],
     entry_points={
